8f108fd582178f6cef6b5b62141b79e791972cd3
Thomas <gaderions@gmail.com>
2021-03-01 18:18:21 +0100

    Layout_listitem V0.1

--- app/src/main/java/fr/sharebookstore/app/MainActivity.java
+++ app/src/main/java/fr/sharebookstore/app/MainActivity.java
@@ -1,78 +1,22 @@
 package fr.sharebookstore.app;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.app.Activity;
-import android.widget.TextView;
+import android.util.Log;
+import android.widget.LinearLayout;
 
-public class MainActivity extends Activity implements Runnable{
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-    private String hiduke="";
-    private int price=0;
-    private String errmsg="";
-    private int ID_Genre=0;
-    private String Name_Genre ="";
+import java.util.ArrayList;
 
-    public void run() {
-        System.out.println("Select Records Example by using the Prepared Statement!");
-        Connection con = null;
-        int count = 0;
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection
-                    ("jdbc:mysql://ls-0f927a463e6d389cf0f567dc4d5a58f8ca59fcd7.cq7na6hxonpd.eu-central-1.rds.amazonaws.com:3306/ShareBook","sharebookuser","uA?BL6P8;t=P-JKl)]Su>L3Gj$[mz0q]");
-            try{
-                String sql;
-                //	  sql
-                //	  = "SELECT `genre`.`ID_Genre`,
-                //    `genre`.`Nom`
-                //FROM `ShareBook`.`genre`;";
-                sql
-                        = "SELECT `genre`.`ID_Genre`, `genre`.`Nom` FROM `ShareBook`.`genre`;";
-                PreparedStatement prest = con.prepareStatement(sql);
-                //prest.setInt(1,1980);
-                //prest.setInt(2,2004);
-                ResultSet rs = prest.executeQuery();
-                while (rs.next()){
-                    ID_Genre = rs.getInt(1);
-                    Name_Genre = rs.getString(2);
-                    count++;
-                    System.out.println(ID_Genre + "\t" + "- " + Name_Genre);
-                }
-                System.out.println("Number of records: " + count);
-                prest.close();
-                con.close();
-            }
-            catch (SQLException s){
-                System.out.println("SQL statement is not executed!");
-                errmsg=errmsg+s.getMessage();
+public class MainActivity extends AppCompatActivity {
 
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            errmsg=errmsg+e.getMessage();
-        }
+    private static final String TAG = "MainActivity";
 
-        handler.sendEmptyMessage(0);
-
-    }
-    private Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-
-            TextView textView = (TextView) findViewById(R.id.textView0);
-            textView.setText("ID="+ID_Genre+" Name="+Name_Genre+" "+errmsg);
-        }
-    };
-
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
 
 
     @Override
@@ -80,14 +24,46 @@ public class MainActivity extends Activity implements Runnable{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Thread thread = new Thread(this);
-        thread.start();
-
-
-
-
+        getImages();
     }
 
+    private void getImages(){
+        Log.d(TAG, "IniImageBitmaps : preparing bitmaps.");
 
+        mImageUrls.add("https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg");
+        mNames.add("Exemple");
+
+        mImageUrls.add("https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg");
+        mNames.add("Exemple");
+
+        mImageUrls.add("https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg");
+        mNames.add("Exemple");
+
+        mImageUrls.add("https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg");
+        mNames.add("Exemple");
+
+        mImageUrls.add("https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg");
+        mNames.add("Exemple");
+
+        mImageUrls.add("https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg");
+        mNames.add("Exemple");
+
+        mImageUrls.add("https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg");
+        mNames.add("Exemple");
+
+        mImageUrls.add("https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg");
+        mNames.add("Exemple");
+
+        initRecyclerView();
+
+    }
+    private void initRecyclerView(){
+        Log.d(TAG,  "initRecyclerView: init recyclerview");
+
+        LinearLayoutManager layoutManager =  new LinearLayoutManager( this, LinearLayoutManager.HORIZONTAL,  false);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter( this, mNames, mImageUrls);
+        recyclerView.setAdapter(adapter);
+    }
 }
---
+++ app/src/main/java/fr/sharebookstore/app/RecyclerViewAdapter.java
@@ -0,0 +1,78 @@
+package fr.sharebookstore.app;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG ="RecyclerViewAdapter";
+
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context context, ArrayList<String> names, ArrayList<String> imageUrls) {
+        mNames = names;
+        mImageUrls = imageUrls;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG,  "onCreateViewHolder: called.");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem,parent,  false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImageUrls.get(position))
+                .into(holder.image);
+
+        holder.titre.setText(mNames.get(position));
+
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on an image:" +mNames.get(position));
+                Toast.makeText(mContext, mNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageUrls.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView titre;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image_book);
+            titre = itemView.findViewById(R.id.titre);
+        }
+    }
+
+}
