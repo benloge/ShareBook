829bce70bb2615580fd73898f3c02d78b78ed889
Thomas <gaderions@gmail.com>
2021-03-02 18:09:30 +0100

    Store V0.1

--- app/src/main/java/fr/sharebookstore/app/controller/StoreActivity.java
+++ app/src/main/java/fr/sharebookstore/app/controller/StoreActivity.java
@@ -1,15 +1,14 @@
 package fr.sharebookstore.app.controller;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-import android.widget.LinearLayout;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -18,19 +17,20 @@ import java.util.ArrayList;
 import fr.sharebookstore.app.R;
 import fr.sharebookstore.app.RecyclerViewAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class StoreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_store);
         setBottomNavigation();
 
         getImages();
     }
+
     private void setBottomNavigation() {
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.activity_main_bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.action_accueil);
+        bottomNavigationView.setSelectedItemId(R.id.action_store);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             public boolean onNavigationItemSelected() {
                 return onNavigationItemSelected();
@@ -42,24 +42,24 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.action_accueil:
                         if (!classname.contains("MainActivity")) {
-                            startActivity(new Intent(MainActivity.this, MainActivity.class));
+                            startActivity(new Intent(StoreActivity.this, MainActivity.class));
                         }
 
                         break;
                     case R.id.action_store:
                         if (!classname.contains("StoreActivity")) {
-                            startActivity(new Intent(MainActivity.this, StoreActivity.class));
+                            startActivity(new Intent(StoreActivity.this, StoreActivity.class));
                         }
 
                         break;
                     case R.id.action_biblio:
                         if (!classname.contains("BiblioActivity")) {
-                            startActivity(new Intent(MainActivity.this, BiblioActivity.class));
+                            startActivity(new Intent(StoreActivity.this, BiblioActivity.class));
                         }
                         break;
                     case R.id.action_panier:
                         if (!classname.contains("PanierActivity")) {
-                            startActivity(new Intent(MainActivity.this, PanierActivity.class));
+                            startActivity(new Intent(StoreActivity.this, PanierActivity.class));
                         }
                         break;
                 }
@@ -67,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     private static final String TAG = "MainActivity";
 
@@ -107,8 +108,8 @@ public class MainActivity extends AppCompatActivity {
         mImageUrls2.add("http://18.159.181.250/image/les_avantures_de_Sherlock_Holmes_Arthur_Conan_Doyle.jpg");
         mNames2.add("Les Aventures de Sherlock Holmes");
 
-        initRecyclerView(R.id.nouveauteView, mNames, mImageUrls);
-        initRecyclerView(R.id.tendanceView, mNames2, mImageUrls2);
+        initRecyclerView(R.id.store_nouveauteView, mNames, mImageUrls);
+        initRecyclerView(R.id.store_tendanceView, mNames2, mImageUrls2);
 
 
     }
