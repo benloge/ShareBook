efd5376bf6a0357fb5151c7f1ca555eb6e8e7503
benloge <benjy.loge@gmail.com>
2021-03-06 15:03:28 +0100

    Page Product Simple Info

--- app/src/main/java/fr/sharebookstore/app/controller/ProductActivity.java
+++ app/src/main/java/fr/sharebookstore/app/controller/ProductActivity.java
@@ -4,29 +4,59 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Point;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.Random;
+
 import fr.sharebookstore.app.R;
 import fr.sharebookstore.app.utils.Navigation;
+import fr.sharebookstore.app.utils.NetworkProduct;
+import fr.sharebookstore.app.utils.NetworkRecyclerViewDocument;
 
 public class ProductActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Random mRandom = new Random();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        Navigation.setBottomNavigation(ProductActivity.this,this,R.id.action_store);
         Navigation.SetTopToolbar(ProductActivity.this, this);
-        GetProduct();
+
+        TextView Text = (TextView) findViewById(R.id.Product_Auteur);
+        Point size = new Point(Text.getWidth(),Text.getHeight());
+        int[] colors = new int[2];
+        for (int i=0; i<2;i++){
+            colors[i]= Color.HSVToColor(255,new float[]{mRandom.nextInt(361),1.0f,1.0f});
+        }
+        LinearGradient gradient = new LinearGradient(0,0,size.x,size.y,Color.HSVToColor(255,new float[]{mRandom.nextInt(361),1.0f,1.0f}),Color.HSVToColor(255,new float[]{mRandom.nextInt(361),1.0f,1.0f}),Shader.TileMode.REPEAT);
+        Shader shade = gradient;
+        Text.getPaint().setShader(shade);
+
+        GetInformation();
+
     }
 
-    private void GetProduct() {
+    private void GetInformation() {
+        String Id = getIntent().getStringExtra("EXTRA_SESSION_ID");
+        String Name = getIntent().getStringExtra("EXTRA_SESSION_NAME");
+        String requeteSimpleDocInfo = "http://18.159.181.250/api/documents.php?id="+Id;
+        new NetworkProduct(this, ProductActivity.this, Id,"SimpleDocInfo").execute(requeteSimpleDocInfo);
 
     }
+
+
 
     @Override
     public void onClick(View v) {
--- app/src/main/java/fr/sharebookstore/app/utils/NetworkProduct.java
+++ app/src/main/java/fr/sharebookstore/app/utils/NetworkProduct.java
@@ -2,12 +2,18 @@ package fr.sharebookstore.app.utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+
+import fr.sharebookstore.app.R;
 
 /**
  * Created by <VOTRE-NOM> on <DATE-DU-JOUR>.
@@ -16,16 +22,24 @@ public class NetworkProduct extends android.os.AsyncTask<String, Void, String> {
     private String myID;
     private Activity myActivity;
     private Context myContext;
-    private String myTitre;
-    private String myImgUrl;
-    private String myParution;
-    private String myNombrePage;
-    private String myLangue;
+    private String myAction;
+    private String mTitre;
+    private String mImgUrl;
+    private String mResume;
+    private String mParution;
+    private String mLangue;
+    private String mAuteur;
+    private String mType;
+    private String mEditeur;
+    private String mCollection;
+    private int mPage;
 
-    public NetworkProduct(Context myContext, Activity myActivity, String ID) {
+
+    public NetworkProduct(Context myContext, Activity myActivity, String ID, String Action) {
         this.myActivity = myActivity;
         this.myContext = myContext;
         this.myID = ID;
+        this.myAction = Action;
     }
 
     public interface Listeners {
@@ -46,16 +60,55 @@ public class NetworkProduct extends android.os.AsyncTask<String, Void, String> {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        switch (myAction) {
+            case "SimpleDocInfo":
+                GetSimpleDocumentInfo(array);
+        }
+
+
+    }
+
+    private void GetSimpleDocumentInfo(JSONArray array) {
         for(int i = 0; i < array.length(); i++)
         {
             try {
                 JSONObject object = array.getJSONObject(i);
-              //  mID.add(object.getInt("0"));
-               // mNames.add(object.getString("Nom"));
+                mTitre = object.getString("Titre");
+                mImgUrl = object.getString("Image");
+                mResume = object.getString("Resume");
+                mParution = object.getString("Date_Parution");
+                mPage = object.getInt("Nombre_Pages");
+                mLangue = object.getString("Langue");
+                mAuteur = object.getString("Auteur_nom");
+                mType = object.getString("Types");
+                mEditeur = object.getString("Editeur");
+                mCollection = object.getString("Collection");
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
+        SetSimpleDocumentInfo();
+
+    }
+
+    private void SetSimpleDocumentInfo() {
+        TextView Titre = (TextView) myActivity.findViewById(R.id.Product_Titre);
+        ImageView Image = (ImageView) myActivity.findViewById(R.id.Product_Image);
+        TextView Auteur = (TextView) myActivity.findViewById(R.id.Product_Auteur);
+        TextView Resume = (TextView) myActivity.findViewById(R.id.Product_Resume);
+        TextView Parution = (TextView) myActivity.findViewById(R.id.Product_Parution);
+        TextView Page = (TextView) myActivity.findViewById(R.id.Product_Page);
+        TextView Langue = (TextView) myActivity.findViewById(R.id.Product_Langue);
+        Titre.append(mTitre);
+        Auteur.append(mAuteur);
+        Resume.append(mResume);
+        Parution.append(mParution);
+        Page.append(String.valueOf(mPage));
+        Langue.append(mLangue);
+        Glide.with(myContext)
+                .asBitmap()
+                .load(mImgUrl)
+                .into(Image);
 
     }
 }
